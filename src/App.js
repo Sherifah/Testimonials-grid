808da59cbff457f44e4c13be3db7b61b8c8d40e8
Sherifah <adelekebolaji0@gmail.com>
2023-06-16 11:58:48 +0100

    [fix] README.md and .gitignore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from './app.module.css'
+import { FaQuoteLeft } from "react-icons/fa"
 
 const App = () => {
     return (
@@ -6,31 +8,34 @@ const App = () => {
             <div className="md:grid grid-cols-4 grid-rows-2 gap-5">
                 {/* First Testimonial */}
                 <div className="bg-primary-100 p-7 md:p-5 relative col-span-2 rounded-lg mb-5 md:mb-0">
-                    <div className="absolute top-0">
-                        <svg  width="104" height="102" xmlns="http://www.w3.org/2000/svg"><path d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z" fill="#A775F1" fillRule="nonzero"/></svg>
-                    </div>
-                    <div className="flex items-center mb-3">
-                        <img src="./images/image-daniel.jpg" className="passport w-8 h-8 md:w-10 md:h-10 rounded-full border-2 border-neutral-100 mr-3" ></img>
-                        <div className="personal-info">
-                            <h1 className="font-semibold text-sm mb-.5 text-primary-white">Daniel Clifford</h1>
-                            <p className="font-medium text-xs text-primary-lighter">Verified Graduate</p>
+                    <div className="absolute top-0 right-10 z-[-10]" >
+                        <img src="/images/bg-pattern-quotation.svg"></img>
+                    </div> 
+                    <div className="z-10">
+                        <div className={`flex items-center mb-3 ${styles.allText}`}>
+                            <img src="./images/image-daniel.jpg" className="passport w-8 h-8 md:w-10 md:h-10 rounded-full border-2 border-neutral-100 mr-3" ></img>
+                            <div className="personal-info">
+                                <h1 className="font-semibold text-sm mb-.5 text-primary-white">Daniel Clifford</h1>
+                                <p className="font-medium text-xs text-primary-lighter">Verified Graduate</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="testimony-headline text-lg leading-tight font-semibold text-primary-white mb-4">
-                        <h2>
-                            I received a job offer mid-course, and the subjects I learned were current, if not more so, 
-                            in the company I joined. I honestly feel I got every penny’s worth.                         
-                        </h2>
-                    </div>
-                    <div className="testimony-content font-medium text-xs leading-5 text-primary-light">
-                        <p>
-                            “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
-                            transition and have heard some people who had an amazing experience here. I signed up 
-                            for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
-                            The next 12 weeks was the best - and most grueling - time of my life. Since completing 
-                            the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”
-                        </p>
-                    </div>
+                        <div className="testimony-headline text-lg leading-tight font-semibold text-primary-white mb-4">
+                            <h2>
+                                I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+                                in the company I joined. I honestly feel I got every penny’s worth.                         
+                            </h2>
+                        </div>
+                        <div className="testimony-content font-medium text-xs leading-5 text-primary-light">
+                            <p>
+                                “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                                transition and have heard some people who had an amazing experience here. I signed up 
+                                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                                The next 12 weeks was the best - and most grueling - time of my life. Since completing 
+                                the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”
+                            </p>
+                        </div>
+                    </div>                
+                    
                 </div>
 
                 {/* Second Testimonial */}
